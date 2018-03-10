c25e2a43ccd6cf07b4d378ec02ce06a0f9a8f7da
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-10 15:55:18 +0100

    refactor js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 'use strict';
 
 $(function() {
-  
-  const carouselList = $('#carousel ul');
-  function moveFirstSlide() {
-    const firstItem = carouselList.find('li:first');
-    const lastItem = carouselList.find('li:last');
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft: '0'});
-  }
-  function changeSlides() {
-    carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
-  }
-  setInterval(changeSlides, 3000);
-
+  setInterval(function(){
+    $('#carousel ul').animate({marginLeft: '-400px'}, 500, function(){
+        $(this).find('li:last').after($(this).find('li:first'));
+        $(this).css('marginLeft', '0');
+    })
+  }, 3000);
 });
+
+// $(function() {
+  
+//   const carouselList = $('#carousel ul');
+//   function moveFirstSlide() {
+//     const firstItem = carouselList.find('li:first');
+//     const lastItem = carouselList.find('li:last');
+//     lastItem.after(firstItem);
+//     carouselList.css({marginLeft: '0'});
+//   }
+//   function changeSlides() {
+//     carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+//   }
+//   setInterval(changeSlides, 3000);
+
+// });
