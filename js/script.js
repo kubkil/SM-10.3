6b16717dbf8d880729e193d8f53f4a7b2cc1441d
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-18 18:46:24 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,53 +1,75 @@
 'use strict';
 
-  
 $(function() {
 
-  let runIntervalId;
+  const pagination = $('.pagination');
+  const photosList = $('.photos-list');
+  let currentSlide = 0; //zmienna wykorzystywana przy przesuwaniu slajdów, mnożona przez szerokość zdjęcia
+  let runIntervalId; //zmienna przechowująca ID setInterval, potrzebna do clearInterval
+
+  function nextSlide() {
+    if(currentSlide > 3) { //dlaczego 3 a nie 4?
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    changeSlide(currentSlide);
+  }
+
+  function previousSlide() {
+    if(currentSlide < 1) {
+      currentSlide = 4;
+    } else {
+      currentSlide--;
+    }
+    changeSlide(currentSlide); //wartość currentSlide podstawiana pod (slide)
+  }
+
+  pagination.on('click', 'li', function() {
+    currentSlide = $(this).index();
+    changeSlide(currentSlide);
+    restartInterval();
+  });
+
+  function restartInterval() {
+    clearInterval(runIntervalId);
+    runInterval();
+  }
 
 // CHANGE SLIDES 
+
   function runInterval() { 
-    runIntervalId = setInterval(function() {
-        $('#carousel ul').animate({marginLeft: '-400px'}, 500, function() {
-            $(this).find('li:last').after($(this).find('li:first'));
-            $(this).css('marginLeft', '0');
-        });
-      }, 5000);
-    }
+    runIntervalId = setInterval(nextSlide, 3000); //set interval podanie funkcji a nie jej wywołanie czyli bez ();
+    } //changeSlide bez nawiasu w console.log podaje kod funkcji bez jej wywołania
 
-  function stopInterval() {
-    clearInterval(runIntervalId);
+  function changeSlide(slide) {
+    pagination.find('.active').removeClass('active'); //bez kropki bo removeClass z definicji dotyczy klasy
+    pagination.find('li').eq(slide).addClass('active');
+    photosList.animate({marginLeft: -400 * slide}, 500);
   }
 
-  runInterval();
+  // function() {
+  //   $(this).find('li:last').after($(this).find('li:first'));
+  //   $(this).css('marginLeft', '0');
+  // };
 
-  $('#carousel').on('mouseenter', stopInterval).on('mouseleave', runInterval);
+  // function stopInterval() {
+  //   clearInterval(runIntervalId);
+  // }
 
-// PAGINATION
-  function changeBtns() {
-    const currentBtn = $('.pagination-btn.active');
-    const nextBtn = currentBtn.next();
-    currentBtn.removeClass('active');
-    nextBtn.addClass('active');
-  }
-
-  setInterval(changeBtns, 5000, function() {
-    find('pagination-btn:last').after('pagination-btn:last').find('pagination-btn:first');    
-  });
+  // $('#carousel').on('mouseenter', stopInterval).on('mouseleave', runInterval);
 
 // PREV NEXT BUTTONS
   $('#btn-prev').click(function() {
-    $('#carousel ul').animate({marginLeft: '0'}, 500, function(){
-      $(this).find('li:first').before($(this).find('li:last'));
-      $(this).css({marginLeft: '-400px'});
-    });
+    previousSlide();
+    restartInterval();
   }); //button-prev click
 
-  $('#btn-next').click(function() {
-    $('#carousel ul').animate({marginLeft: '-400px'}, 500, function(){
-      $(this).find('li:last').after($(this).find('li:first'));
-      $(this).css({marginLeft: 0});
-    });
+  $('#btn-next').click(function(){
+    nextSlide();
+    restartInterval();
   }); //button-next click
+
+  runInterval();
 
 }); // $( document ).ready()
